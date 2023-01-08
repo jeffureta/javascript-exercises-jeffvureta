31b3aa0a07000c31ae4f64baa9d6e2eb0a7351e7
jeffureta <jeffvincentureta@gmail.com>
2023-01-08 13:29:43 +0000

    Finished exercises 01 to 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % (4 * 100) === 0))
 };
 
 // Do not edit below this line
